0a24accef3d67e2d401fa93d6a1235b6fb0e59a2
Kirill Bondarev <Gut54566@yandex.ru>
2021-03-16 02:23:01 +0700

    Created network service and DTO

--- app/src/main/java/ru/mrfiring/fscurrencies/data/network/DataTransferObjects.kt
+++ app/src/main/java/ru/mrfiring/fscurrencies/data/network/DataTransferObjects.kt
@@ -1,2 +1,24 @@
 package ru.mrfiring.fscurrencies.data.network
 
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class CurrenciesContainer(
+    @Json(name = "Date") val date: String,
+    @Json(name = "PreviousDate") val previousDate: String,
+    @Json(name = "PreviousURL") val previousUrl: String,
+    @Json(name = "Timestamp") val timestamp: String,
+    @Json(name = "Valute") val valute: Map<String, Currency>
+)
+
+@JsonClass(generateAdapter = true)
+data class Currency(
+    @Json(name = "ID") val id: String,
+    @Json(name = "NumCode") val numCode: String,
+    @Json(name = "CharCode") val charCode: String,
+    @Json(name = "Nominal") val nominal: Int,
+    @Json(name = "Name") val name: String,
+    @Json(name = "Value") val value: Double,
+    @Json(name = "Previous") val previousValue: Double
+)
--- app/src/main/java/ru/mrfiring/fscurrencies/data/network/Service.kt
+++ app/src/main/java/ru/mrfiring/fscurrencies/data/network/Service.kt
@@ -1,2 +1,10 @@
 package ru.mrfiring.fscurrencies.data.network
 
+import retrofit2.http.GET
+
+const val BASE_URL = "https://www.cbr-xml-daily.ru/"
+
+interface CurrenciesService{
+    @GET("daily_json.js")
+    suspend fun getCurrenciesList(): CurrenciesContainer
+}
