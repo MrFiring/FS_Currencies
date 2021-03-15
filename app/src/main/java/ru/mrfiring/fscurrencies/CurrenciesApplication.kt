61ff958bb7ba947b9bdd1bcf2afcb84108349155
Kirill Bondarev <Gut54566@yandex.ru>
2021-03-16 01:48:46 +0700

    Hilt annotations added

--- app/src/main/java/ru/mrfiring/fscurrencies/CurrenciesApplication.kt
+++ app/src/main/java/ru/mrfiring/fscurrencies/CurrenciesApplication.kt
@@ -1,7 +1,8 @@
 package ru.mrfiring.fscurrencies
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
-
+@HiltAndroidApp
 class CurrenciesApplication: Application() {
 }
--- app/src/main/java/ru/mrfiring/fscurrencies/presentation/MainActivity.kt
+++ app/src/main/java/ru/mrfiring/fscurrencies/presentation/MainActivity.kt
@@ -2,8 +2,10 @@ package ru.mrfiring.fscurrencies.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
 import ru.mrfiring.fscurrencies.R
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
