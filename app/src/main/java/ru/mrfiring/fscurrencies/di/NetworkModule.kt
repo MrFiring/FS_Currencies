fa127bf02c8eb1713ee9d20b71935ebdb2b6c8d4
Kirill Bondarev <Gut54566@yandex.ru>
2021-03-16 02:23:23 +0700

    BaseModules implementation

---
+++ app/src/main/java/ru/mrfiring/fscurrencies/di/NetworkModule.kt
@@ -0,0 +1,29 @@
+package ru.mrfiring.fscurrencies.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import ru.mrfiring.fscurrencies.data.network.BASE_URL
+import ru.mrfiring.fscurrencies.data.network.CurrenciesService
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(): Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(MoshiConverterFactory.create())
+        .build()
+
+    @Singleton
+    @Provides
+    fun provideStockService(retrofit: Retrofit): CurrenciesService {
+        return retrofit.create(CurrenciesService::class.java)
+    }
+}
