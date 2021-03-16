d73ebfce4719e329bedd8cf016fdc34d1b61fa1f
Kirill Bondarev <Gut54566@yandex.ru>
2021-03-16 10:19:00 +0700

    Room and DI DB module implemented

--- app/src/main/java/ru/mrfiring/fscurrencies/data/database/Room.kt
+++ app/src/main/java/ru/mrfiring/fscurrencies/data/database/Room.kt
@@ -1,2 +1,37 @@
 package ru.mrfiring.fscurrencies.data.database
 
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import kotlinx.coroutines.selects.select
+
+@Dao
+interface CurrencyDao{
+
+    @Transaction
+    @Query("select * from databasecontainer")
+    fun getContainerWithCurrencies(): LiveData<DatabaseContainerWithCurrencies>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertContainer(item: DatabaseContainer)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAllCurrencies(items: List<DatabaseCurrency>)
+
+    @Query("select count(*) from databasecontainer")
+    fun getContainersCount(): Int
+
+    @Query("delete from databasecontainer")
+    fun deleteAllContainers()
+
+    @Query("delete from databasecurrency")
+    fun deleteAllCurrencies()
+
+}
+
+@Database(entities = [
+    DatabaseContainer::class,
+    DatabaseCurrency::class
+], version = 1)
+abstract class CurrencyDatabase: RoomDatabase(){
+    abstract val currencyDao: CurrencyDao
+}
