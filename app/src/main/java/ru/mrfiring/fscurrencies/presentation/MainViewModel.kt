dd0628746a3ac031edd067f5ed6feb5e9bf9ac03
Kirill Bondarev <Gut54566@yandex.ru>
2021-03-16 11:14:18 +0700

    MainViewModel implemented

---
+++ app/src/main/java/ru/mrfiring/fscurrencies/presentation/MainViewModel.kt
@@ -0,0 +1,49 @@
+package ru.mrfiring.fscurrencies.presentation
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import ru.mrfiring.fscurrencies.domain.DomainContainerWithCurrencies
+import ru.mrfiring.fscurrencies.domain.FetchCurrenciesUseCase
+import ru.mrfiring.fscurrencies.domain.GetContainerWithCurrenciesLiveDataUseCase
+import java.io.IOException
+import javax.inject.Inject
+
+enum class LoadingStatus{
+    LOADING, ERROR, DONE
+}
+
+class MainViewModel @Inject constructor(
+    application: Application,
+    private val getContainerWithCurrenciesLiveDataUseCase: GetContainerWithCurrenciesLiveDataUseCase,
+    private val fetchCurrenciesUseCase: FetchCurrenciesUseCase
+): AndroidViewModel(application) {
+
+    val container: LiveData<DomainContainerWithCurrencies> =
+        getContainerWithCurrenciesLiveDataUseCase()
+
+    private val _status = MutableLiveData<LoadingStatus>()
+    val status: LiveData<LoadingStatus>
+    get() = _status
+
+
+    init {
+        fetchData()
+    }
+
+    private fun fetchData(fromCache: Boolean = true) = viewModelScope.launch {
+        try {
+            _status.value = LoadingStatus.LOADING
+            fetchCurrenciesUseCase(fromCache)
+            _status.value = LoadingStatus.DONE
+
+        }catch (ex: IOException){
+            ex.printStackTrace()
+        }
+    }
+
+    fun onUpdateData() = fetchData(false)
+}
