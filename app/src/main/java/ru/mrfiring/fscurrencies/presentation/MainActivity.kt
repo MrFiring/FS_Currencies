c51880378c4ea280549f9e30672f722bb3a22227
Kirill Bondarev <Gut54566@yandex.ru>
2021-03-16 12:53:49 +0700

    BasePrototype implemented

--- app/src/main/java/ru/mrfiring/fscurrencies/presentation/MainActivity.kt
+++ app/src/main/java/ru/mrfiring/fscurrencies/presentation/MainActivity.kt
@@ -2,13 +2,31 @@ package ru.mrfiring.fscurrencies.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
 import dagger.hilt.android.AndroidEntryPoint
-import ru.mrfiring.fscurrencies.R
+import ru.mrfiring.fscurrencies.databinding.ActivityMainBinding
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+
+        val adapter = CurrenciesRecyclerViewAdapter(viewModel::onItemClick)
+
+        binding.currencyList.adapter = adapter
+
+        viewModel.container.observe(this, Observer {
+            if(it != null){
+                adapter.submitList(it.currencies)
+            }
+        })
+
+        setContentView(binding.root)
     }
 }
