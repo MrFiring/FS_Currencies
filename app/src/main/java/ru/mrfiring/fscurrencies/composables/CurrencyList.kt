0972ef5ebc9de956f58decfd317dea6c11a32e36
Kirill Bondarev <Gut54566@yandex.ru>
2021-12-25 00:29:21 +0700

    Ui visibility improvements

--- app/src/main/java/ru/mrfiring/fscurrencies/composables/CurrencyList.kt
+++ app/src/main/java/ru/mrfiring/fscurrencies/composables/CurrencyList.kt
@@ -1,17 +1,17 @@
 package ru.mrfiring.fscurrencies.composables
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import ru.mrfiring.fscurrencies.R
 import ru.mrfiring.fscurrencies.domain.DomainCurrency
 
 @Composable
@@ -31,15 +31,21 @@ internal fun CurrencyItem(item: DomainCurrency, onClick: (DomainCurrency) -> Uni
             .fillMaxWidth()
             .clickable { onClick(item) }
     ) {
-        Row {
-            Column(modifier = Modifier.weight(0.7f)) {
-                Text(text = item.charCode)
-                Text(text = item.name)
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier.padding(all = 8.dp)
+        ) {
+            Column {
+                Text(text = item.name, fontWeight = FontWeight.Bold)
+                Text(text = item.charCode, fontWeight = FontWeight.Light)
             }
-            Column(modifier = Modifier.weight(0.3f)) {
-                Text(text = item.value.toString())
+            Column {
+                Text(text = stringResource(id = R.string.price_format, item.value))
                 val delta = item.getDeltaPrice()
-                Text(text = delta.toString(), color = if (delta > 0) Color.Green else Color.Red)
+                Text(
+                    text = stringResource(id = R.string.price_delta_format, delta),
+                    color = if (delta > 0) Color.Green else Color.Red
+                )
             }
         }
     }
